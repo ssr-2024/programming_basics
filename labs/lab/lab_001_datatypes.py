06f3cb1eeb63f63a20934a04d90877e0c2c65146
Jonathan Rudolf <jonathan.rudolf@students.unibe.ch>
2024-10-01 10:47:28 +0200

    solve exercise 1 and 2 in lab_001_datatypes.py

--- labs/lab/lab_001_datatypes.py
+++ labs/lab/lab_001_datatypes.py
@@ -1,28 +1,28 @@
 # @exercise_1
-exercise1_a = ''
-exercise1_b = ''
-exercise1_c = ''
-exercise1_d = ''
+exercise1_a = 'YesYesYes'
+exercise1_b = 'NoNoNoNoNoNoNoNoNo'
+exercise1_c = '12 * 3 = {12 * 3}'
+exercise1_d = '12 * 3 = 36'
 
 # @exercise_2a
 goals = 3
-print('I have scored ' + goals + ' goals.')
+print(f'I have scored {goals} goals.')
 
 # @exercise_2b
-size = input('my size [m]: ')
-weight = input('my weight [kg]: ')
+size = float(input('my size [m]: '))
+weight = float(input('my weight [kg]: '))
 bmi = weight / (size**2)
-print('My BMI is ' + bmi + '.')
+print(f'My BMI is {bmi}.')
 
 # @exercise_2c
 text = input('Text to repeat: ')
-repeat_n = input('How often should "' + text + '" be repeated? ')
+repeat_n = int(input('How often should "' + text + '" be repeated? '))
 print(text * repeat_n)
 
 # @exercise_2d
-a = input('Enter a number: ')
+a = int(input('Enter a number: '))
 a_times_3 = a * 3
-print(a + ' times 3 = ' + a_times_3)
+print(f'{a} times 3 = {a_times_3}')
 
 # @exercise_3a
 
