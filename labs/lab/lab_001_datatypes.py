537e68be6938c260054d7f50c01a54aaaa747e1a
Eliane <eliane.hintermann@students.unibe.ch>
2024-10-01 10:46:10 +0200

    solve exercise 1 by assigning the correct strings to each variable

--- labs/lab/lab_001_datatypes.py
+++ labs/lab/lab_001_datatypes.py
@@ -1,12 +1,12 @@
 # @exercise_1
-exercise1_a = ''
-exercise1_b = ''
-exercise1_c = ''
-exercise1_d = ''
+exercise1_a = 'YesYesYes'
+exercise1_b = 'NoNoNoNoNoNoNoNoNo'
+exercise1_c = '12 * 3 = {12 * 3}'
+exercise1_d = '12 * 3 = 36'
 
 # @exercise_2a
 goals = 3
-print('I have scored ' + goals + ' goals.')
+print('I have scored ' + str(goals) + ' goals.')
 
 # @exercise_2b
 size = input('my size [m]: ')
