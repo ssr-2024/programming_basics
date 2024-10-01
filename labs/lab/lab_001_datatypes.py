e8127fdd520ff85d72f273940b9939abab3c6156
robin sch├ñrer <robin.schaerer@students.unibe.ch>
2024-10-01 10:47:21 +0200

    solve exercise 1 and exercise 2a

--- labs/lab/lab_001_datatypes.py
+++ labs/lab/lab_001_datatypes.py
@@ -1,12 +1,12 @@
 # @exercise_1
-exercise1_a = ''
-exercise1_b = ''
-exercise1_c = ''
-exercise1_d = ''
+exercise1_a = 'YesYesYes'
+exercise1_b = 'NoNoNoNoNoNoNoNoNo'
+exercise1_c = '12 * 3 = {12 * 3}'
+exercise1_d = '12 * 3 = 36' # f macht das in dem string die geschweiften klammern '{}' ausgeführt ausgeführt wird.
 
 # @exercise_2a
 goals = 3
-print('I have scored ' + goals + ' goals.')
+print('I have scored ' + str(goals) + ' goals.') # variable 'goals' wird durch den befehl str() in einen string umwandeln
 
 # @exercise_2b
 size = input('my size [m]: ')
