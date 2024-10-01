20a6eb0b71c7836f9a7b5cc412df71cd85743505
matthieschaller <matthieu.schaller@students.unibe.ch>
2024-10-01 11:52:34 +0200

    create a function which creates a zigzag line and executes a small demo

---
+++ Jump.py
@@ -0,0 +1,18 @@
+import turtle as t
+
+def jump():
+    t.left(60)
+    t.forward(100)
+    t.right(120)
+    t.forward(100)
+    t.left(60)
+
+
+
+
+t.forward(100)
+jump()
+t.forward(100)
+
+
+t.exitonclick
