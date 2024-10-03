d5f5dc444645ff565eedc0481b83b3e68a17feb3
Nicolas Iseli <nicolas.iseli@students.unibe.ch>
2024-10-03 15:10:27 +0200

    include the solution for the exercise 002.2.a. It is about when does a variable changes its value

--- labs/lab/lab_002_variables.py
+++ labs/lab/lab_002_variables.py
@@ -1,12 +1,18 @@
 from math import pi
 
 # @exercise_1
-name = Iseli
-vorname= Nicolas
+name = "Iseli"
+vorname= "Nicolas"
 
 # @exercise_2a
 tore = None
-
+tore = 2
+tore + 1
+# der Wert von tore wird zuerst von "None" auf 2 gesetzt. Bei "tore +1"
+# wird der Wert "tore" kurzfristig auf 3 gesetzt => 2+1=3 wobei eine bessere Formulierung
+# wäre, dass es den Wert nur ausrechnet. Dieser Wert wird 
+# jedoch nicht gespeichert. Dieses Ereignis wird nicht gespeichert oder der 
+# der Variablen zugewiesen. Der Wert tore=2 bleibt erhalten. 
 # @exercise_2b
 
 tore = 2
