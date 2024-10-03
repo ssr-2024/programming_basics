fb30558a4cfb7575ae551148b2e4f3e44ee8c55b
Nicolas Iseli <nicolas.iseli@students.unibe.ch>
2024-10-03 14:52:16 +0200

    contain the script for the UML "Hello, Howdy or Greetings"

--- labs/lab/lab_101_uml_to_code.py
+++ labs/lab/lab_101_uml_to_code.py
@@ -1,1 +1,11 @@
 # @exercise_uml
+def begruessung(spam):
+    if spam == 1:
+        return "Hello"
+    if spam == 2:
+        return "Howdy"
+    else:
+        return "Greetings"
+    
+spam = int(input("Geben sie 1, 2 oder eine andere beliebige Zahl ein für eine Begrüssung: "))
+begruessung(spam)
