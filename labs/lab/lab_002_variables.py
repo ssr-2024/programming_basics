4b5465dde065d299c77e651f29cbbab2c4fcbb64
Eliane <eliane.hintermann@students.unibe.ch>
2024-10-04 16:17:25 +0200

    solve exercise 2a (lab_002) which includes notes for overwriting a variable

--- labs/lab/lab_002_variables.py
+++ labs/lab/lab_002_variables.py
@@ -7,6 +7,16 @@ vorname = "Eliane"
 
 # @exercise_2a
 tore = None
+tore = 2
+tore + 1
+
+print(tore)
+
+#After running the script:
+# tore = None
+# tore = 2
+# tore + 1
+# the variable tore will hold the value 2. The initial assignment of None is overwritten by tore = 2. However, the line tore + 1 simply performs a calculation without updating the value of tore or displaying any output.
 
 # @exercise_2b
 
