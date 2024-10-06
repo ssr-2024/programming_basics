608e6a382b53308a5b55c21941b821cf5333a56d
Nicolas Iseli <nicolas.iseli@students.unibe.ch>
2024-10-06 15:43:33 +0200

    include the solution to the exercise 003.1. It is about different error messages.

--- labs/lab/lab_003_errors.py
+++ labs/lab/lab_003_errors.py
@@ -1,5 +1,12 @@
 # @exercise_1a 'TypeError: can only concatenate str (not "float") to str'
-
+text = "Das Ergebnis ist: " + 3.14 
 # @exercise_1b "TypeError: unsupported operand type(s) for ** or pow(): 'str' and 'int'"
-
+result = "3" ** 2
 # @exercise_1c 'ZeroDivisionError: division by zero'
+result = 10 / 0
+
+"""
+'some ' + 'text' = a
+the correct Error message is: SyntaxError: can't assign to operator.
+the 2nd
+"""
