41e22f1936a3791cad055051844c41369dd69e42
robin sch├ñrer <robin.schaerer@students.unibe.ch>
2024-10-07 20:39:49 +0200

    solve lab_002 completely

--- labs/lab/lab_002_variables.py
+++ labs/lab/lab_002_variables.py
@@ -2,15 +2,24 @@ from math import pi
 
 # @exercise_1
 
+name = 'Musterman'
+vorname = 'Max'
+
 
 # @exercise_2a
-tore = None
+tore = 2
 
 # @exercise_2b
 
 tore = 2
-tore + 1
+tore += 1
 
 # @exercise_3_kreisflaeche
 
-radius = 5
+from math import pi
+
+radius = 7
+
+flaeche = pi  * radius ** 2
+
+print((f"Die Fläche des Kreises mit Radius {radius} ist: {flaeche}"))
