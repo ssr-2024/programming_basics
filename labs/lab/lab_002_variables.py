b5b62b45a73f52d0f219f9ece0f263295974beab
hj1911 <janis.holzer@students.unibe.ch$>
2024-10-08 10:49:31 +0200

    exercise 2

--- labs/lab/lab_002_variables.py
+++ labs/lab/lab_002_variables.py
@@ -1,16 +1,19 @@
 from math import pi
 
 # @exercise_1
+name = "Holzer"
+vorname = "Janis"
 
 
 # @exercise_2a
-tore = None
+tore = 2 #bleibt 2
 
 # @exercise_2b
-
-tore = 2
-tore + 1
+tore = 2 
+tore += 1
 
 # @exercise_3_kreisflaeche
+from math import pi
 
 radius = 5
+print(f'{radius**2 * pi}')
