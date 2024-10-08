49e0feed06e04d7f34b6a90321280247118d485e
Johanna Heitmann <johanna.heitmann@students.unibe.ch>
2024-10-08 13:56:32 +0200

    solve lab002 exercises by defining and calculating with variables

--- labs/lab/lab_002_variables.py
+++ labs/lab/lab_002_variables.py
@@ -1,16 +1,21 @@
 from math import pi
 
 # @exercise_1
-
+name='heitmann'
+vorname='johanna'
 
 # @exercise_2a
-tore = None
+tore = 2
 
 # @exercise_2b
 
 tore = 2
-tore + 1
+tore=tore + 1
 
 # @exercise_3_kreisflaeche
 
 radius = 5
+fläche=(radius*radius) * pi
+
+print(fläche)
+
