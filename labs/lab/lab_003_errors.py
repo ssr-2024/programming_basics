25923e0cd6bd13eda691ebb45e9af4c6e645f262
Johanna Heitmann <johanna.heitmann@students.unibe.ch>
2024-10-08 14:39:46 +0200

    solve lab003 exercises by prompting the respective errors

--- labs/lab/lab_003_errors.py
+++ labs/lab/lab_003_errors.py
@@ -1,5 +1,10 @@
 # @exercise_1a 'TypeError: can only concatenate str (not "float") to str'
+var1='Johanna'
+var2=2.5
+result=var1+var2
 
 # @exercise_1b "TypeError: unsupported operand type(s) for ** or pow(): 'str' and 'int'"
+result2='Johanna'**2
 
 # @exercise_1c 'ZeroDivisionError: division by zero'
+result3=5/0
