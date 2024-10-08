c92e1618cd15846a20a889f6b737890fe2c413fa
Nicolas Iseli <nicolas.iseli@students.unibe.ch>
2024-10-08 14:49:35 +0200

    include the solution for the exercise 102. Drawing triangle with stars

--- labs/lab/lab_102_loops.py
+++ labs/lab/lab_102_loops.py
@@ -1,6 +1,21 @@
+from typing import NoReturn
 
-def triangle():
-    print('')
+def triangle(n: int) -> NoReturn:
+    """Prints a pattern of stars in the shape of an ascending and descending triangle.
 
+    Parameters
+    ----------
+    n (int): 
+        The number of lines in the ascending part of the star pattern.
 
-triangle()
+    Returns:
+    ----------
+    NoReturn: This function does not return a value.
+    """
+    for i in range(1, n + 1):
+        print("* " * i)
+    for i in range(n - 1, 0, -1):
+        print("* " * i)
+
+# Example call:
+triangle(5)
