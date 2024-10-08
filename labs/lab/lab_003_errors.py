e58a959d6d1319caef62ad6838abd09421a82483
hj1911 <janis.holzer@students.unibe.ch$>
2024-10-08 12:58:07 +0200

    lab_003 exercises

--- labs/lab/lab_003_errors.py
+++ labs/lab/lab_003_errors.py
@@ -1,5 +1,6 @@
 # @exercise_1a 'TypeError: can only concatenate str (not "float") to str'
-
+raise TypeError('can only concatenate str (not "float") to str')
 # @exercise_1b "TypeError: unsupported operand type(s) for ** or pow(): 'str' and 'int'"
-
+raise TypeError("unsupported operand type(s) for ** or pow(): 'str' and 'int'")
 # @exercise_1c 'ZeroDivisionError: division by zero'
+raise ZeroDivisionError('division by zero')
