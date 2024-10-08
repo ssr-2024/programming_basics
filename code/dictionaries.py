64a87c42e691fe5dce00112cd1023eaaced955bd
robin sch├ñrer <robin.schaerer@students.unibe.ch>
2024-10-08 11:36:33 +0200

    various changes to dictionaries.py and jump.py

--- code/dictionaries.py
+++ code/dictionaries.py
@@ -1,4 +1,22 @@
-def print_result(name,percentage):
+from typing import *
+
+def print_result(name: str,percentage: Union[int,float])-> None:
+    """prints the name and the corresponding precentage value nicely formatted.
+
+    Parameters
+    -----------
+
+    name: str, char
+        name of the individual or test case to be displayed.
+
+    percentage: int, float
+        percentage score to be visualized as asterisks
+
+    Returns
+    --------
+
+        none    
+    """
     print(f"{name}: {'*'*(percentage//10)}")
 
 successful_tests = {
@@ -6,7 +24,7 @@ successful_tests = {
     'Matthieu': 55,
     'Jonathan': 63,
     'Robin': 78,
-    'Nicolas1': 72,
+    'Nicolas1': 72, # Werte können hier auch andere Datentypen annehmen, beispielsweise ein float also hier 72.8 Dazu muss dann oben "percentage" in einen integer umgewandelt werden, damit die integer division funktioniert. Dictionaries sind sehr flexibles, anders als Listen.
     'Eliane': 77,
     'Johanna': 82,
     'Nicolas2': 87,
