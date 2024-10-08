184802f6bc0d4c79fd86bbff67c2c0c4807fcfee
Elias <elias.kehrli@students.unibe.ch>
2024-10-08 11:26:31 +0200

    updated docstring

--- code/dictionaries.py
+++ code/dictionaries.py
@@ -1,5 +1,16 @@
+
 def print_result (name,percentage):
+    """Prints the name and corresponding percentage value in a nicely formatted format.
+    
+    Parameters:
+        name (str): Name of the person.
+        percentage (int or float): Percentage of successful tests visualized by stars (1 star for each 10%).
+
+    Returns:
+        None
+    """
     print(f"{name}: {'*'*(percentage//10)}")
+
 
 successful_tests = {
     'Janis' : 44,
