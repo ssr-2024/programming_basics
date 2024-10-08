f7234a068f25272700f31adce14b9d44f9d1650f
Elias <elias.kehrli@students.unibe.ch>
2024-10-08 10:50:20 +0200

    update the procent visualization with adding a list

--- code/procent_visual.py
+++ code/procent_visual.py
@@ -1,5 +1,9 @@
-percentages = [33, 50, 22, 70, 12, 90]
+def visualize (percentage):
+    print (f"{percentage}%: {'*' * (percentage // 10)}")
+    ''' Diese Funktion visualisiert den Prozentsatz, den sie als Argument erhält.
+        Sie gibt eine Zeile mit Sternchen aus, die die Prozentzahl darstellt.
+        Aber es gibt nur ganze Sterne, keine halben. '''
+
+percentages = [44, 33, 22]
 for percentage in percentages:
     visualize(percentage)
-
-
