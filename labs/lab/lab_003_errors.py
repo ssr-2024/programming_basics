e79c4bbe2d8c76716370a5151bb5ef9bf7fc27a7
Jonathan Rudolf <jonathan.rudolf@students.unibe.ch>
2024-10-12 14:08:25 +0200

    solves lab 003

--- labs/lab/lab_003_errors.py
+++ labs/lab/lab_003_errors.py
@@ -1,5 +1,9 @@
 # @exercise_1a 'TypeError: can only concatenate str (not "float") to str'
-
+s1 = "string"
+s2 = 2.0
+s3 = s1 + s2 # this leads to the error
 # @exercise_1b "TypeError: unsupported operand type(s) for ** or pow(): 'str' and 'int'"
-
+a = "string"
+a_squared = a**2
 # @exercise_1c 'ZeroDivisionError: division by zero'
+divide_by_zero = 10/0
