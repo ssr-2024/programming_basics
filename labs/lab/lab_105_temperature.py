800092c03ae587c99cde6cba6e09bfb8cac7942f
Jonathan Rudolf <jonathan.rudolf@students.unibe.ch>
2024-10-14 10:25:47 +0200

    solves lab_105 step 1

--- labs/lab/lab_105_temperature.py
+++ labs/lab/lab_105_temperature.py
@@ -20,4 +20,13 @@ def unit_output(selection):
     return ''
 
 ## Skript
+unit = int(input("Welche Umrechnung willst du machen? \n"
+                 + "(1) Umrechnung von Celsius nach Kelvin \n"
+                 + "(2) Umrechnung von Celsius nach Fahrenheit \n"
+                 + "(3) Umrechnung von Kelvin nach Celsius \n"
+                 + "(4) Umrechnung von Kelvin nach Fahrenheit \n"
+                 + "(5) Umrechnung von Fahrenheit nach Celsius \n"
+                 + "(6) Umrechnung von Fahrenheit nach Kelvin \n"))
+
+
 print(f'{12}° {unit_input(12)} ≅ {12}° {unit_output(12)}')
