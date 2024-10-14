cda06b68c8e9a4e61fb598300e8dbe7244161d2a
Eliane <eliane.hintermann@students.unibe.ch>
2024-10-14 21:41:48 +0200

    solve lab 003_errors by writing code that causes errors

--- labs/lab/lab_003_errors.py
+++ labs/lab/lab_003_errors.py
@@ -1,5 +1,12 @@
 # @exercise_1a 'TypeError: can only concatenate str (not "float") to str'
 
+"So gross bin ich " + 1.75
+
 # @exercise_1b "TypeError: unsupported operand type(s) for ** or pow(): 'str' and 'int'"
 
+"100" ** 2
+
 # @exercise_1c 'ZeroDivisionError: division by zero'
+
+100/0
+
