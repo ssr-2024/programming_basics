89c6029cb509d26e4b58185f0360ae9b2cab885d
Nicolas <nicolas.morath@unibe.ch>
2024-10-15 10:42:48 +0200

    add demo code for dicts and percentages

--- code/dictionaries.py
+++ code/dictionaries.py
@@ -1,19 +1,45 @@
 
-def print_result(name,percentage):
+
+from typing import *
+
+
+
+
+def print_result(name: str, percentage: Union [int, float]) -> None:
+    """
+    Prints a name and a bar visualization of the percentage using asterisks.
+
+    Each asterisk represents 10%. For example, a percentage of 44 will result in four asterisks.
+
+    Parameters
+    ----------
+    name : str
+        The name of the individual or test case to be displayed.
+    percentage : int
+        The percentage score to be visualized as asterisks.
+    
+    Returns
+    --------
+
+            none
+    
+    """
     print(f"{name}: {'*' * (percentage // 10)}")
 
+
 sucessfull_tests = {
-    'Janis':44,
-    'Matthieu':55,
-    'Jonathan':63,
-    'Robin':68,
-    'Elias':99,
-    'Nicolas1':20,
-    'Nicolas2':7
+    'Janis': 44,
+    'Matthieu': 55,
+    'Jonathan': 63,
+    'Robin': 68,
+    'Elias': 99,
+    'Nicolas1': 20,
+    'Nicolas2': 7
 }
 
-for name,percentage in sucessfull_tests.items():
-    print_result(name,percentage)
+for name, percentage in sucessfull_tests.items():
+    print_result(name, percentage)
+
 
 
 
--- code/percentage.py
+++ code/percentage.py
@@ -1,5 +1,11 @@
+
 def visualize(persentage):
     print(f"{persentage}%: {'*' * (persentage // 10)}")
+
+
+
+
+
 
 percentages = [33, 50, 22, 70, 12, 90]
 for persentage in percentages:
