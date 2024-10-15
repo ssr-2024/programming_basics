730ec4d54e5e7dc96ab73ba9e587b09dbd64b85a
Elias <elias.kehrli@students.unibe.ch>
2024-10-15 13:55:37 +0200

    add solution for lab_201 Exercise 1,2 & 3

--- labs/lab/lab_201_numpy.py
+++ labs/lab/lab_201_numpy.py
@@ -1,10 +1,21 @@
 import numpy as np
 
 # @exercise_1
-
+array = np.arange(10)
+print(array)
 # @exercise_2
-
+array = np.full((4, 3), 4)
+print(array)
 # @exercise_3
+array = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+array = array[array % 2 != 0]
+'''Boolean Indexing: 
+The expression array % 2 != 0 creates 
+a boolean array where each element is True 
+if the corresponding element in array 
+is odd, and False if it is even.
+'''
+print(array)
 
 # @exercise_4
 
