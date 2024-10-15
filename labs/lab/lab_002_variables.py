7619e2b202fd1e281ba87b2bba3ba5321d5e3017
Nicolas <nicolas.morath@unibe.ch>
2024-10-15 10:52:53 +0200

    add completed lab test for lab_002_variables.py

--- labs/lab/lab_002_variables.py
+++ labs/lab/lab_002_variables.py
@@ -2,15 +2,27 @@ from math import pi
 
 # @exercise_1
 
+vorname = "Nicolas"
+name = "Morath"
 
 # @exercise_2a
-tore = None
+tore = 2
+# Die Variable tore enthält noch immer den Wert 2, da ihr in der 2. Zeile kein Wert zugewiesen wird
 
 # @exercise_2b
 
 tore = 2
-tore + 1
+tore = tore + 1
 
 # @exercise_3_kreisflaeche
 
-radius = 5
+from math import pi
+
+# Definition des Kreisradius 
+radius = 4.32
+
+# Berechnung der Kreisfläche
+area = pi * radius ** 2
+
+# Anzeige des Resultats
+print(f'The area of the circle with radius {radius} is {area}')
