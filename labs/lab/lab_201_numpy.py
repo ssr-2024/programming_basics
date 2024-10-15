899f989d4d412f8915538011067e1578a422faa5
Johanna Heitmann <johanna.heitmann@students.unibe.ch>
2024-10-15 11:51:58 +0200

    solve lab201 exercises 1 to 4 by using np.arrange and np.full to create desired output

--- labs/lab/lab_201_numpy.py
+++ labs/lab/lab_201_numpy.py
@@ -1,15 +1,21 @@
 import numpy as np
 
 # @exercise_1
+array = np.arange(10)
 
 # @exercise_2
+array = np.full((4, 3), 4)
 
 # @exercise_3
+array=np.arange(1,10,2)
 
 # @exercise_4
+array = np.arange(10)
+array[np.arange(1,10,2)]=-1
 
 # @exercise_5
 init = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+array[init]
 
 # @exercise_reshape
 
