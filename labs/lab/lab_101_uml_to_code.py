d344031befbd92f903239941b69d84d850e9a421
Nicolas Iseli <nicolas.iseli@students.unibe.ch>
2024-10-18 13:09:30 +0200

    include the fixed code for the UML exercise 101.

--- labs/lab/lab_101_uml_to_code.py
+++ labs/lab/lab_101_uml_to_code.py
@@ -1,11 +1,12 @@
 # @exercise_uml
 def begruessung(spam):
     if spam == 1:
-        return "Hello"
-    if spam == 2:
-        return "Howdy"
+        print("Hello")
+    elif spam == 2:
+        print("Howdy")
     else:
-        return "Greetings"
-    
+        print("Greetings!")
+
 spam = int(input("Geben sie 1, 2 oder eine andere beliebige Zahl ein für eine Begrüssung: "))
+
 begruessung(spam)
