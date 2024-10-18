f449508f7a273d8f3ecd0ace213de8fae4d69d51
Eliane <eliane.hintermann@students.unibe.ch>
2024-10-18 12:24:54 +0200

    solve exercise 5, exercise_reshape and exercise_stack of lab 201_numpy by using the reshape()-function and the vstack()-function

--- labs/lab/lab_201_numpy.py
+++ labs/lab/lab_201_numpy.py
@@ -20,8 +20,19 @@ array [np.arange(1,10,2)] = -1
 # @exercise_5
 init = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
 
+array = init.copy() #creates a copy from *init* to modify it 
+array [np.arange(0,9,2)] = -1
+
+
 # @exercise_reshape
 
+array = np.arange(10).reshape(2,5)
+
 # @exercise_stack
+a = np.array([[0, 1, 2, 3, 4], [0, 0, 0, 0, 0]])
+b = np.array([[0, 1, 1, 0, 1], [0, 1, 0, 1, 0]])
+
+array = np.vstack((a,b))
 
 # @exercise_range
+
