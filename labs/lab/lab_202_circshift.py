7d1d9672a6cb1f8b28b595021e982e2e8e76535d
Nicolas Iseli <nicolas.iseli@students.unibe.ch>
2024-10-19 15:32:55 +0200

    include the solution to the exercise 202. It is about local and global scope and how to play with lists.

--- labs/lab/lab_202_circshift.py
+++ labs/lab/lab_202_circshift.py
@@ -50,13 +50,28 @@ def reset():
 
 def circshift_left_to_right():
     global heart
-    # do the circshift left to the heart
+    heart = [row[-1:] + row[:-1] for row in heart]  # Shift each row to the left
+'''
+row[-1:] nimmt das letzte Element der aktuellen Zeile.
 
+row[:-1] nimmt alle Elemente der aktuellen Zeile, außer das letzte.
+
+Durch die Kombination row[-1:] + row[:-1] wird das letzte Element 
+an den Anfang der neuen Zeile verschoben, und der Rest der Zeile folgt.
+'''
 
 def circshift_top_to_bottom():
     global heart
-    # do the circshift top_bottom to the heart
+    heart = heart[-1:] + heart[:-1]  # Shift the entire grid upwards
+'''
+heart[-1:] nimmt die letzte Zeile der heart-Matrix.
 
+heart[:-1] nimmt alle Zeilen der Matrix, außer die letzte.
+
+Durch die Kombination heart[-1:] + heart[:-1] wird die 
+letzte Zeile an den Anfang der neuen Matrix verschoben, 
+während die anderen Zeilen folgen.
+'''
 
 if __name__ == '__main__':
     global fig, handle
