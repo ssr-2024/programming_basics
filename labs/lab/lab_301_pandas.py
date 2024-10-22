f05efe345f1b907016cec199b67839caf66af560
Eliane <eliane.hintermann@students.unibe.ch>
2024-10-22 11:47:51 +0200

    create the experiment setup of the lab 301_pandas by creating a dictionary, which will be translated to a dataframe. This dataframe is then saved as an excel file.

--- labs/lab/lab_301_pandas.py
+++ labs/lab/lab_301_pandas.py
@@ -2,9 +2,32 @@ import numpy as np
 import pandas as pd
 from pathlib import Path
 
-
 def experiment_setup(xlsx_path):
+    # Data for the experiment setup
+    data = {
+            'vpn01': {
+                'Gruppe': 'A',
+                'Trial_01': 'vid_01.mp4',
+                'Trial_02': 'vid_02.mp4',
+                'Trial_03': 'vid_03.mp4',
+                'Trial_04': 'vid_04.mp4'
+            },
+            'vpn02': {
+                'Gruppe': 'B',
+                'Trial_01': 'vid_03.mp4',
+                'Trial_02': 'vid_04.mp4',
+                'Trial_03': 'vid_01.mp4',
+                'Trial_04': 'vid_02.mp4'
+            }
+        }
+    
+    # Create a DataFrame
+    df = pd.DataFrame(data).transpose()
+    
+    # Save the DataFrame as an Excel file
+    df.to_excel(xlsx_path)
     return None
+
 
 
 def load_relevant_data(file_name):
@@ -14,3 +37,6 @@ def load_relevant_data(file_name):
 if __name__ == '__main__':
     experiment_setup('output.xlsx')
     load_relevant_data('labs/lab/data/lab301_sport/data.csv')
+    
+
+
