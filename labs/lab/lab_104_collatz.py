fe56b1c705a3c39702258b5d8f4fdd4304020a71
hj1911 <janis.holzer@students.unibe.ch$>
2024-10-22 13:08:59 +0200

    commenting lab_104, but i'm not sure, that one could write it simpler

--- labs/lab/lab_104_collatz.py
+++ labs/lab/lab_104_collatz.py
@@ -1,11 +1,37 @@
-def collatz(a):
-    if a % 2 == 0:
-        a = a//2 
-    else:
+def collatz(a:int):
+    """
+    function to caculate the next collatz-number
+
+    Parameters
+    ----------
+    a: int
+        the input number
+
+    Returns
+    -------
+    int
+        the following collatz-number
+
+    """
+    if a % 2 == 0:#if a is even
+        a = a//2 #returns integer
+    else:#if a is uneven
         a = a*3+1
     return a
 
 def run():
+    """
+    calculates the collatz-numbers, until 1 is reached 3 times
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    list    
+        list of collatz-numbers 
+    """
     liste = []
     a = int(input("Please enter a number"))
     liste.append(a)
@@ -19,5 +45,5 @@ def run():
     print(liste)
     return liste
 
-if __name__ == '__main__':
+if __name__ == '__main__': #is run, if not imported 
     run()
