457db5ad8be265e58300469724d3423e9d760fe0
robin sch├ñrer <robin.schaerer@students.unibe.ch>
2024-10-22 09:16:37 +0200

    solve lab_101

--- labs/lab/lab_101_uml_to_code.py
+++ labs/lab/lab_101_uml_to_code.py
@@ -1,1 +1,12 @@
 # @exercise_uml
+
+# Eingabe des Wertes von spam durch den Benutzer. Mit int() wird der Wert in eine Ganzzahl umgewandelt. Und mit input() wird der Wert vom Benutzer abgefragt.
+spam = int(input("Bitte gib einen Wert zwischen 1 und 3 ein.: "))
+
+# Der eingegebene Wert wird überprüft und die entsprechende Ausgabe wird ausgegeben.
+if spam == 1:
+    print("Hello")
+elif spam == 2:
+    print("Howdy")
+else:
+    print("Greetings!")
