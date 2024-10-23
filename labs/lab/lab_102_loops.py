0c453e0a32928e9fd6fb3fd4da68fdd1038ee30c
robin sch├ñrer <robin.schaerer@students.unibe.ch>
2024-10-23 12:32:11 +0200

    solve lab_102

--- labs/lab/lab_102_loops.py
+++ labs/lab/lab_102_loops.py
@@ -1,6 +1,29 @@
 
-def triangle():
-    print('')
+def triangle(size):
+    """Prints a symmetric triangle pattern using asterisks, with a top and bottom part.
 
+    Parameters
+    -----------
 
-triangle()
+    size: int
+        The number of lines in the top part of the triangle, and also determines the overall height of the triangle.
+
+    Returns
+    --------
+
+        None
+    """    
+    # Erste Schleife: Erstellt den oberen Teil des Dreiecks
+    # Die Schleife startet bei 1, da die erste Zeile nur ein Stern enthält
+    # Für jeden Wert von i wird i-mal die Zeichenkette '*   ' gedruckt, um die Anzahl der Sterne pro Zeile zu erhöhen
+    for i in range(1, size + 1):
+        print('*   ' * i)
+    
+    # Zweite Schleife: Erstellt den unteren Teil des Dreiecks
+    # Die Schleife startet bei (size - 1) und läuft bis 1 (exklusiv der 0)
+    # Für jeden Wert von i wird i-mal die Zeichenkette '*   ' gedruckt, um die Anzahl der Sterne pro Zeile zu verringern
+    for i in range(size - 1, 0, -1):
+        print('*   ' * i)
+
+triangle(5)
+print()
