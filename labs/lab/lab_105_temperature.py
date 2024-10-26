197a4fb917c20ff836bc4e47eafd0fb245e6818d
Nicolas <nicolas.morath@unibe.ch>
2024-10-26 14:02:01 +0200

    add 1. step in lab_105_temperature.py

--- labs/lab/lab_105_temperature.py
+++ labs/lab/lab_105_temperature.py
@@ -1,7 +1,7 @@
 ## Funktionen
 
 def celsius_to_kelvin(temperatur):
-    return temperatur
+    return temperatur + 273.15
 
 def kelvin_to_celsius(temperatur):
     return temperatur
@@ -12,12 +12,25 @@ def celsius_to_fahrenheit(temperatur):
 def fahrenheit_to_celsius(temperatur):
     return temperatur
 
-
 def unit_input(selection):
     return ''
 
 def unit_output(selection):
     return ''
 
-## Skript
-print(f'{12}° {unit_input(12)} ≅ {12}° {unit_output(12)}')
+## Auswahl des Benutzers für die Umrechnung
+print("Wählen Sie die gewünschte Umrechnung:")
+print("    (1) Umrechnung von Celsius nach Kelvin")
+print("    (2) Umrechnung von Celsius nach Fahrenheit")
+print("    (3) Umrechnung von Kelvin nach Celsius")
+print("    (4) Umrechnung von Kelvin nach Fahrenheit")
+print("    (5) Umrechnung von Fahrenheit nach Celsius")
+print("    (6) Umrechnung von Fahrenheit nach Kelvin")
+
+# Speichere die Auswahl des Benutzers als int
+selection = int(input("Ihre Auswahl: "))
+
+
+
+
+
