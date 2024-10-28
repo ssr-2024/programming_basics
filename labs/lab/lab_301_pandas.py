5025edf1759b83b007b29e25abeedfa2587a7580
hj1911 <janis.holzer@students.unibe.ch$>
2024-10-28 11:02:47 +0100

    lab_301 finished

--- labs/lab/lab_301_pandas.py
+++ labs/lab/lab_301_pandas.py
@@ -2,15 +2,49 @@ import numpy as np
 import pandas as pd
 from pathlib import Path
 
-
 def experiment_setup(xlsx_path):
+    path = Path(xlsx_path)
+    data = {
+        'vpn01': {
+            'Gruppe': 'A',
+            'Trial_01': 'vid_01.mp4',
+            'Trial_02': 'vid_02.mp4',
+            'Trial_03': 'vid_03.mp4',
+            'Trial_04': 'vid_04.mp4'
+        },
+        'vpn02': {
+            'Gruppe': 'B',
+            'Trial_01': 'vid_03.mp4',
+            'Trial_02': 'vid_04.mp4',
+            'Trial_03': 'vid_01.mp4',
+            'Trial_04': 'vid_02.mp4'
+        }
+    }
+
+    df = pd.DataFrame(data)
+    df = df.transpose()
+    df.to_excel(path)
     return None
 
 
 def load_relevant_data(file_name):
-    return None
+    # Importiere die Datei mit dem `;`-Trennzeichen und überspringe die ersten beiden Zeilen
+    df = pd.read_csv(file_name, sep=';', skiprows=2, usecols=[0, 1, 2, 3, 4, 5], na_values="UNKNOWN")
+    
+    # Spalten auswählen
+    df = df[["country code", "height [cm]", "weight [kg]", "main sport"]]
+   
+    # Spaltennamen anpassen
+    df = df.rename({"country code":"country", "height [cm]":"height", "weight [kg]":"weight", "main sport":"sport"}, axis="columns")
+    
+    # Zeilen mit fehlenden Werten entfernen
+    df.dropna(inplace=True)
+    
+    return df
 
 
 if __name__ == '__main__':
     experiment_setup('output.xlsx')
     load_relevant_data('labs/lab/data/lab301_sport/data.csv')
+
+    
