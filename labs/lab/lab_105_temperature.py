0bdcb31586611ca6677f5c540893f835a61a485c
Johanna Heitmann <johanna.heitmann@students.unibe.ch>
2024-10-29 17:18:54 +0100

    add code asking for user input to lab 105

--- labs/lab/lab_105_temperature.py
+++ labs/lab/lab_105_temperature.py
@@ -20,4 +20,13 @@ def unit_output(selection):
     return ''
 
 ## Skript
+# Fragt Nutzer*innen nach der Umrechnung, die sie durchführen möchten
+user_input = int(input('Please enter the number corresponding to the conversion you want to do:\n'
+                            '(1) Celsius to Kelvin\n'
+                            '(2) Celsius to Fahrenheit\n'
+                            '(3) Kelvin to Celsius\n'
+                            '(4) Kelvin to Fahrenheit\n'
+                            '(5) Fahrenheit to Celsius\n'
+                            '(6) Fahrenheit to Kelvin\n'
+                            'Your choice: '))
 print(f'{12}° {unit_input(12)} ≅ {12}° {unit_output(12)}')
