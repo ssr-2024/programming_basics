bdedb0fcd8a391ce25335f228f0ef528f89e56bf
Johanna Heitmann <johanna.heitmann@students.unibe.ch>
2024-10-31 18:49:31 +0100

    add a decision for temperature conversion to code for lab 105

--- labs/lab/lab_105_temperature.py
+++ labs/lab/lab_105_temperature.py
@@ -13,6 +13,13 @@ def celsius_to_fahrenheit(temperatur):
 def fahrenheit_to_celsius(temperatur):
     return (temperatur-32) * 5/9
 
+# Löst das Problem der fehlenden Funktionen zur Umrechnung von Kelvin und Fahrenheit (und umgekehrt)
+def kelvin_to_fahrenheit(temperatur):
+    return (temperatur - 273.15) * 9/5 + 32
+
+def fahrenheit_to_kelvin(temperatur):
+    return (temperatur - 32) * 5/9 + 273.15
+
 # 2. Funktion, welche basierend auf der Eingabe des Benutzers die Einzugebende Einheit zurückgibt.
 def unit_input(selection): # Schleifen!
      if selection == 1:
@@ -48,6 +55,19 @@ selection = int(input('Please enter the number corresponding to the conversion y
 # 3. Fragt Nutzer*innen nach der Temperatur, die sie umrechnen möchten
 temperatur = float(input(f'Please enter a temperature in {unit_input(selection)}: '))
 
+# 6. Transformation der Temperatur
+if selection == 1:
+    converted_temp=celsius_to_kelvin(temperatur)
+elif selection == 2:
+    converted_temp=celsius_to_fahrenheit(temperatur)
+elif selection == 3:
+    converted_temp=kelvin_to_celsius(temperatur)
+elif selection == 4:
+    converted_temp=kelvin_to_fahrenheit(temperatur) 
+elif selection == 5:
+    converted_temp=fahrenheit_to_celsius(temperatur)
+elif selection == 6:
+    converted_temp=fahrenheit_to_kelvin(temperatur)
 
 
 
