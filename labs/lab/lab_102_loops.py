0fba52551eae0bb086d4d53e1ce53254df7a1e78
Eliane <eliane.hintermann@students.unibe.ch>
2024-11-01 09:41:53 +0100

    solve lab102 by drawing a triangle of stars with a given size by defining a function that uses two for-loops

--- labs/lab/lab_102_loops.py
+++ labs/lab/lab_102_loops.py
@@ -1,6 +1,33 @@
+def triangle(size: int) --> None:
+    """Draws a symmetrical triangle of stars with the given size.
 
-def triangle():
-    print('')
+    This function prints an upper and a lower triangle made of asterisks ('*') 
+    that together form a diamond-like shape. The upper triangle has a growing 
+    number of asterisks in each row, while the lower triangle has a decreasing 
+    number.
 
+    Parameters
+    ----------
+    size: int
+        The number of rows in the upper triangle and the height of the 
+        diamond-like shape (2 * size - 1). The lower triangle will have 
+        one row less than the specified size.
+    
+    Returns
+    ----------
+    None
+       
+    """
+    
+    # Loop 1: Draws the upper triangle
+    for i in range(size):
+        print('* ' * (i + 1))
+    
+    # Loop 2: Draws the lower triangle
+    for i in range(size - 1):
+        print('* ' * (size - i - 1))
 
-triangle()
+# Call the function
+triangle(1)
+triangle(2)
+triangle(5)
