571a4234a197a3311a1ae30984a5c2eaea11142e
eliaskehrli <elias.kehrli@students.unibe.ch>
2024-11-04 20:19:30 +0100

    add the solutions for lab_003

--- labs/lab/lab_003_errors.py
+++ labs/lab/lab_003_errors.py
@@ -1,5 +1,6 @@
 # @exercise_1a 'TypeError: can only concatenate str (not "float") to str'
-
+'Alice' + 5.2
 # @exercise_1b "TypeError: unsupported operand type(s) for ** or pow(): 'str' and 'int'"
-
+'Hello' ** 3
 # @exercise_1c 'ZeroDivisionError: division by zero'
+3 / 0
