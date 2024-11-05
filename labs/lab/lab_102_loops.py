f0e30538a12a83eb2a1326f5c6833d4ff09d61d1
Nicolas Iseli <nicolas.iseli@students.unibe.ch>
2024-11-05 09:19:45 +0100

    include the fix for the exercise 102. Change the code so that it use only one loop

--- labs/lab/lab_102_loops.py
+++ labs/lab/lab_102_loops.py
@@ -1,30 +1,10 @@
-'''
-from typing import NoReturn
-
-def triangle(n: int) -> NoReturn:
-    """Prints a pattern of stars in the shape of an ascending and descending triangle.
-
-    Parameters
-    ----------
-    n (int): 
-        The number of lines in the ascending part of the star pattern.
-
-    Returns:
-    ----------
-    NoReturn: This function does not return a value.
-    """
-    for i in range(1, n + 1):
-        print("* " * i)
-    for i in range(n - 1, 0, -1):
-        print("* " * i)
-
-triangle(5)
-'''
-
 def triangle(size):
-    for i in range(1, 2 * size):
-        # Erhöhe bis n und dann verringere die Anzahl der Sterne
-        stars = i if i <= size else 2 * size - i
-        print("*  " * 1)
+    for i in range(1, size + 1):
+        # Berechne die Anzahl der Leerzeichen und Sterne
+        spaces = size - i
+        stars = 2 * i - 1
+        # Zeichne die Leerzeichen und Sterne
+        print(" " * spaces + "*" * stars)
 
-triangle(5)
+triangle(n)
+n=int(input("gib eine Zahl ein"))
