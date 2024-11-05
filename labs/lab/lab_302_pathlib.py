e62e6d5b07e6f7dc54ece729de29237ea3b5c19c
Johanna Heitmann <johanna.heitmann@students.unibe.ch>
2024-11-05 15:38:28 +0100

    solve lab302 by implementing  functions using pathlib to create and return dictionaries containing tha targeted data

--- labs/lab/lab_302_pathlib.py
+++ labs/lab/lab_302_pathlib.py
@@ -1,9 +1,9 @@
 from pathlib import Path
-from typing import Union, Dict
+from typing import Union, Dict # um zu spezifizieren, ob str oder Path direkt eingelesen werden solll
 
 
 # erwartet entweder str oder path und gibt vor, was er als Rückgabewert erwartet
-def vpns_exp01(path: Union[str, Path]) -> Dict[str, Path]:
+def vpns_exp01(path: Union[str, Path] = 'labs/lab/data/exp01') -> Dict[str, Path]:
     """
     Returns a dictionary containing the experiment setup based on
     all CSV files in the provided path.
@@ -38,10 +38,16 @@ def vpns_exp01(path: Union[str, Path]) -> Dict[str, Path]:
     }
     ```
     """
-    
+    path = Path(path) # konvertiert den Pfad in ein Path-Objekt
+    exp_setup = {} # erstellt ein leeres dictionary
+    for file in path.glob('*.csv'): # sucht alle csv-Dateien im angegebenen Pfad, geht auch mit iterdir()
+        name=file.stem  
+        exp_setup[name]=file # fügt den Dateinamen und den Pfad zur Datei in das dictionary ein
+    return exp_setup
 
 
-def vpns_exp02(path: Union[str, Path]) -> Dict[str, Dict[int, Path]]:
+
+def vpns_exp02(path: Union[str, Path]= 'labs/lab/data/exp02') -> Dict[str, Dict[int, Path]]:
     """
     Returns a dictionary containing the experiment setup based on
     the folder structure of the provided path and the containing CSV files.
@@ -94,9 +100,19 @@ def vpns_exp02(path: Union[str, Path]) -> Dict[str, Dict[int, Path]]:
     }
     ```
     """
+    path = Path(path) # Path konvertieren
+    exp_setup = {}
+    for vpn_folder in path.iterdir():
+        if vpn_folder.is_dir():
+            vpn_name = vpn_folder.name
+            exp_setup[vpn_name] = {} # erstellt ein leeres dictionary für die VPNs
+            for csv_file in vpn_folder.glob('*.csv'):
+                mzp = int(csv_file.stem.split('_')[-1])
+                exp_setup[vpn_name][mzp] = csv_file
+    return exp_setup
     
 
 
 if __name__ == '__main__':
-    vpns_exp01('labs/lab/data/exp01')
-    vpns_exp02('labs/lab/data/exp02')
+    print(vpns_exp01('labs/lab/data/exp01'))
+    print(vpns_exp02('labs/lab/data/exp02'))
