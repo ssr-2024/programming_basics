837c8f9af61138223a98a57ca14184076b52c897
Matthieu Schaller <matthieu.schaller@students.unibe.ch>
2024-12-09 16:36:49 +0100

    fehlerbehebung

--- labs/lab/lab_201_numpy.py
+++ labs/lab/lab_201_numpy.py
@@ -5,20 +5,18 @@ array = np.arange(10)
 print(array)
 
 # @exercise_2
-import numpy as np
 
 array = np.full((4, 3), 4)
 print(array)
 
 
 # @exercise_3
-import numpy as np
 
 array = np.arange(10)
 
 # Extrahiere die ungeraden Zahlen
-odd_numbers = array[array % 2 == 1]
-print(odd_numbers)
+array = array[array % 2 == 1]
+
 
 # @exercise_4
 array = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
